d6f988668fe38b7c5c3b7836acf1b6b1d5346e30
BaeJunhyeoung <bjh0919035@gamil.com>
2021-06-03 21:21:47 +0900

    testtttttttttt abc

--- app/src/main/java/com/example/muansmartteam/MainActivity.kt
+++ app/src/main/java/com/example/muansmartteam/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         //testestsetsettttttttttt
-        // test
+        // test abc
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
